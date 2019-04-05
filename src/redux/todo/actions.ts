1e06972a4c45be57cf884b1742135d9eeea649fd
klequis <klequis@gmail.com>
2019-04-05 12:39:22 -0700

    added todoDeleteRequest

--- src/redux/todo/actions.ts
+++ src/redux/todo/actions.ts
@@ -3,7 +3,8 @@ import {
   TODOS_READ_REQUEST_KEY,
   TODOS_READ_KEY,
   TODOS_ADD_REQUEST_KEY,
-  TODOS_READ_BY_ID_REQUEST_KEY
+  TODOS_READ_BY_ID_REQUEST_KEY,
+  TODOS_DELETE_REQUEST_KEY
 } from './constants'
 import { createRequestThunk } from '../action-helpers'
 import api from '../../api'
@@ -104,4 +105,11 @@ export const todoAddRequest = createRequestThunk({
   key: TODOS_ADD_REQUEST_KEY,
   success: [todoAdd],
   failure: [(error: Error) => console.log('(7) todoAddRequestCall: request failed', error)]
+})
+
+export const todoDeleteRequest = createRequestThunk({
+  request: api.todos.delete,
+  key: TODOS_DELETE_REQUEST_KEY,
+  success: [todosReadRequest],
+  failure: [(error: Error) => console.log('(7) todoDeleteRequestCall: request failed', error)]
 })
